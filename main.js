3367094ac96af2c6cfe86e7b0f108393ed0e65d2
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-25 13:24:06 -0400

    added some music and keyframes

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const yesBtn = document.querySelector('.yesBtn')
-yesBtn.addEventListener('click', function (event) {
-  prompt('Enter your name!');
+const enterBtn = document.querySelector('.enterBtn')
+enterBtn.addEventListener('click', function (event) {
+  var name = prompt('Enter your name!');
+  if (name != null) {
+    document.querySelector('.player-name').innerHTML =
+    'Welcome ' + name;
+  }
 });
+enterBtn.addEventListener('click',function play(event){
+        let audio = document.getElementById("audio");
+        audio.play();
+});
+// function play(){
+//       let audio = document.getElementById("audio");
+//       audio.play();
+//     }
