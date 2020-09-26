008a7d8a65bc7474d4d6dd3bdb5c10ab6f5ea380
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-26 13:57:20 -0400

    battle function almost done

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ enterBtn.addEventListener('click',function play(event){
 });
 
 
-const heros = [{
+const herosArr = [{
     name: 'Knight',
     health: 150,
     attackPower: 10
@@ -32,22 +32,37 @@ const heros = [{
   }
 ];
 
-document.querySelectorAll('.hero').forEach(hero => {
-  hero.addEventListener('click', event => {
+const  henchmenArr = [{
+      name: 'Ghoul',
+      health: 10,
+    },
+    {
+      name: 'Orc',
+      health: 20,
+    },
+    {
+      name: 'Dark Elf',
+      health: 30,
+    },
+    {
+      name: 'Troll',
+      health: 50,
+    }
+  ];
+
+document.querySelectorAll('.hero').forEach(heroInfo => {
+  heroInfo.addEventListener('click', event => {
     // console.log('here', hero);
-    const selectionValue = hero.dataset.type;
+    const selectionValue = heroInfo.dataset.type;
     // console.log(selectionValue);
-    const selectedHero = heros.filter(hero => hero.name.toLowerCase() === selectionValue.toLowerCase())[0];
+    const selectedHero = herosArr.filter(heroInfo => heroInfo.name.toLowerCase() === selectionValue.toLowerCase())[0];
     console.log(selectedHero);
-    game.hero = new Hero(selectedHero);
-    game.battle();
+    hero = new Hero(selectedHero);
+    battle();
   });
 });
 
-// hero1.addEventListener('click', event => {
-//   game.hero = new Hero(heros[0]);
-//   game.battle();
-// });
+
 
 
 class Player {
@@ -71,7 +86,7 @@ class Hero extends Player {
   }
   attack(player) {
     player.health -= this.attackPower;
-
+    console.log(player);
   }
 }
 
@@ -93,6 +108,7 @@ class Henchman extends Villan {
   }
   attack(player) {
     player.health -= Math.floor(Math.random() * 11);
+    console.log(player);
   }
 }
 class Boss extends Villan {
@@ -104,63 +120,79 @@ class Boss extends Villan {
   }
   attack(player) {
     player.health -= Math.floor(Math.random() * 21);
+    console.log(player);
   }
 }
 
 
 
+function battle() {
+  const henchman = new Henchman(henchmenArr[Math.floor(Math.random() * 4)]);
 
-
-class Game {
-  constructor() {
-
+  const attackButton = document.querySelector('.attack');
+  attackButton.addEventListener('click', event => {
+  hero.attack(henchman);
+  henchman.attack(hero);
+  if(hero.health <= 0) {
+   console.log('You have been defeated!')
+      attackButton.style.display = "none";
+  } else if(henchman.health <= 0) {
+    console.log('Enemy has been defeated!')
+    attackButton.style.display = "none";
   }
 
-  setHero() {
-    var num = 3
-    let heros = [{
-        name: 'Knight',
-        health: 150,
-        attackPower: 10
-      },
-      {
-        name: 'Archer',
-        health: 125,
-        attackPower: 12
-      },
-      {
-        name: 'Wizard',
-        health: 100,
-        attackPower: 15
-      }
-    ];
-    if (num == 1 /*knight is selected*/ ) {
-      this.hero = new Hero(heros[0]);
-    } else if (num == 2 /*archer is selected*/ ) {
-      this.hero = new Hero(heros[1]);
-    } else if (num == 3 /*wizard is selected*/ ) {
-      this.hero = new Hero(heros[2]);
-    }
-
-  }
-
-  setVillan() {
-    villans
-    this.villan = new(villans[0]);
-  }
-
-  battle() {
-    console.log(this.hero);
-  }
+});
 }
 
 
-
-const game = new Game();
-// let herohealth = document.getElementById("hero-health-bar")
-// herohealth.value -= 10;
-// let enemyhealth = document.getElementById("enemy-health-bar")
-// enemyhealth.value -= 10;
+// class Game {
+//   constructor() {
+//
+//   }
+//
+//   setHero() {
+//     var num = 3
+//     let heros = [{
+//         name: 'Knight',
+//         health: 150,
+//         attackPower: 10
+//       },
+//       {
+//         name: 'Archer',
+//         health: 125,
+//         attackPower: 12
+//       },
+//       {
+//         name: 'Wizard',
+//         health: 100,
+//         attackPower: 15
+//       }
+//     ];
+//     if (num == 1 /*knight is selected*/ ) {
+//       this.hero = new Hero(heros[0]);
+//     } else if (num == 2 /*archer is selected*/ ) {
+//       this.hero = new Hero(heros[1]);
+//     } else if (num == 3 /*wizard is selected*/ ) {
+//       this.hero = new Hero(heros[2]);
+//     }
+//
+//   }
+//
+//   setVillan() {
+//     villans
+//     this.villan = new(villans[0]);
+//   }
+//
+//   battle() {
+//     console.log(this.hero);
+//   }
+// }
+//
+// const hero1 = document.querySelector('.hero1');
+//
+//
+//
+// const game = new Game();
 
   document.querySelectorAll('.hero').forEach(hero => {
   hero.addEventListener('click', event => {
@@ -170,4 +202,3 @@ const game = new Game();
       topnav.style.display ='block';
   });
 });
-// game.setHero();
